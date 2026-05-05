0bf5082309ec0119974f5c339f127a48fa33bd1b
sophiabianca21 <sophiahaldos2@gmail.com>
2026-05-05 10:00:08 +0800

    layouts using navigation setup

---
+++ app/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+         <v-list-item
+            prepend-icon="mdi-home-account"
+            title="Home"
+            value="home"></v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Student Portal</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+     <NuxtPage />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                 <v-row>
+                    <v-col>
+                        <v-text-field
+                        
+                        ></v-text-field>
+                    </v-col>
+                 </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
